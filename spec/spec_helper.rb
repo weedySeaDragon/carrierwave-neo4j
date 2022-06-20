d85594e9ef39082db2d1327d3fe574ab7398d88a
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2022-06-19 18:53:18 -0700

    spec_helper: added consts for username, pwd; moved to make easier to spot

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,13 @@ require "helpers/fake_migrations"
 require "carrierwave"
 require "carrierwave/active_graph"
 
+
+NEO4J_USERNAME = ENV['NEO4J_DB_USER'] || 'neo4j'
+NEO4J_PASSWORD =  ENV['NEO4J_DB_PASSWORD'] || 'password'
+SERVER_URL = ENV['NEO4J_URL'] || 'bolt://localhost:7687'
+
+# ----------------------------------------------------------------------------
+
 def file_path(*paths)
   File.expand_path(File.join(File.dirname(__FILE__), "fixtures", *paths))
 end
@@ -28,13 +35,12 @@ end
 CarrierWave.root = public_path
 # DatabaseCleaner[:neo4j, connection: {type: :bolt, path: 'bolt://localhost:7006'}].strategy = :transaction
 
-server_url = ENV['NEO4J_URL'] || 'bolt://localhost:7687'
 ActiveGraph::Base.driver =
-    Neo4j::Driver::GraphDatabase.driver(server_url, Neo4j::Driver::AuthTokens.none, encryption: false)
+    Neo4j::Driver::GraphDatabase.driver(SERVER_URL, Neo4j::Driver::AuthTokens.basic(NEO4J_USERNAME, NEO4J_PASSWORD), encryption: false)
 
 RSpec.configure do |config|
   config.before(:each) do
-    DatabaseCleaner.avoid_validation do 
+    DatabaseCleaner.avoid_validation do
       DatabaseCleaner.clean
       FilesystemCleaner.clean
       FakeMigrations.migrate(:up)
